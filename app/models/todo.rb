cb6422a65692fbdab676a8fe5e2a6e2fdeeb72fb
a_bahaa <ahmedbahaa_11@hotmail.com>
2018-03-21 15:40:18 +0200

    Add comments

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,3 @@
 class Todo < ApplicationRecord
-  belongs_to :list, touch: true
+  belongs_to :list, touch: true # this is going to touch the parent, the list, and then update it's updated_at timestamp.
 end
